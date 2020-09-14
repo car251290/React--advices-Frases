b843ef828fb09806da83696630faea5f182b9d93
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-09-14 16:19:01 -0700

    commit

--- App.js
+++ App.js
@@ -1,43 +1,68 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import {useState,useEffect} from 'react'
+const App = () => {
+    const [advice, setAdvice] = useState([]);
 
-class App extends React.Component {
-    state = {advice:'',
-}
+    const fetchAdvice = async(advice) => {
+        const response = await axios.get(`https://api.adviceslip.com/${advice}`);
+          setAdvice (response.data.slip);
 
-    componentDidMount(){
-        this.fetchAdvice();
+          //setAdvice('advice');
+          //this.setState({advice});
+          setAdvice({advice});
+
+        
     }
-    //FECTH THE API FOR THE ADVICE 
-    fetchAdvice = () => {
-        //GET THE IPA
-        axios.get('https://api.adviceslip.com/advice')
-        .then((response)=>{
-           const{advice} = response.data.slip;
+    useEffect((advice)=>{
+      fetchAdvice({advice})
+    },[advice])
 
-           this.setState({advice});
-
-        })
-        .catch((error)=>{
-            console.log(error)
-
-        });
-    }
-    render(){
-        //for get the advice in this.state
-        const {advice} = this.state;
 
         return(
             <div className='app'>
             <div className='card'>
               <h1 classname='heading'>{advice}</h1>
-              <button className = "button" onClick={this.fetchAdvice}>
+              <button className = "button" onClick={()=> setAdvice({advice})} >
                   <span>Give me the best advice!</span>
               </button>
             </div>
             </div>
         );
-    }
+    
 }
+
+
+//class App extends React.Component {
+  
+ //   state = { 
+   //     advice:'',
+//}
+
+  //  componentDidMount(){
+    //    this.fetchAdvice();
+    //}
+    //FECTH THE API FOR THE ADVICE 
+    //fetchAdvice = () => {
+        //GET THE IPA
+    //    axios.get('https://api.adviceslip.com/advice')
+     //   .then((response)=>{
+     //      const{advice} = response.data.slip;
+
+     //      this.setState({advice});
+
+     //   })
+     //   .catch((error)=>{
+     //       console.log(error)
+
+     //   });
+   // }
+   // render(){
+        //for get the advice in this.state
+    //    const {advice} = this.state;
+
+       
+   // }
+//}
 export default App;
